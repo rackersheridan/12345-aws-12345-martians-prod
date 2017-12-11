8307c95f5bb83a3dab86c97c043090a2a6ac83ee
Sheridan Roberts <sheridan.roberts@rackspace.com>
2017-12-11 12:47:21 -0600

    further learning on terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-module "base_module" {
-  source = "./base-network"
-  environment = "training"
-  name = "base_training_module"
-}
-
 provider "aws" {
   allowed_account_ids = ["219831553792"]
   region              = "us-east-2"
@@ -15,4 +9,16 @@ terraform {
     key     = "terraform.tfstate"
     encrypt = "false"
   }
+}
+
+module "base_module" {
+  source = "./base-network"
+  environment = "training"
+  name = "base_training_module"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-aa1b34cf"
+  subnet_id = "${module.base_module.public_subnets[0]}"
+  instance_type = "t2.micro"
 }
