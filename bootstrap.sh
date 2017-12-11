9f73b2d2e59f9fb41b6acf5c59f8fe9b128d0dcb
Sheridan Roberts <sheridan.roberts@rackspace.com>
2017-12-11 14:40:03 -0600

    multiple instances across subnets/azs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
-apt-get update; 
-apt-get install -y docker; 
+sleep 120;
+apt-get update;
+apt-get install -y docker;
+ufw disable;
 docker run -dit --name my-apache-app -p 80:80 httpd
--- main.tf
+++ main.tf
@@ -1,25 +1,67 @@
 provider "aws" {
-  allowed_account_ids = ["219831553792"]
-  region              = "us-east-2"
+  allowed_account_ids           = ["219831553792"]
+  region                        = "us-east-2"
 }
 
 terraform {
   backend "s3" {
-    bucket  = "sheridanterraformtraining"
-    key     = "terraform.tfstate"
-    encrypt = "false"
+    bucket                      = "sheridanterraformtraining"
+    key                         = "terraform.tfstate"
+    encrypt                     = "false"
   }
 }
 
 module "base_module" {
-  source = "./base-network"
-  environment = "training"
-  name = "base_training_module"
+  source                        = "./base-network"
+  environment                   = "training"
+  name                          = "base_training_module"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent                   = true
+
+  filter {
+    name                        = "name"
+    values                      = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name                        = "virtualization-type"
+    values                      = ["hvm"]
+  }
+
+  owners                        = ["099720109477"]
+}
+
+resource "aws_security_group" "web_sg" {
+  name                          = "allow-web"
+  description                   = "allow traffic to 80"
+  vpc_id                        = "${module.base_module.vpc_id}"
+
+  ingress {
+    from_port                   = 80
+    to_port                     = 80
+    protocol                    = "tcp"
+    cidr_blocks                 = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port                   = 0
+    to_port                     = 65535
+    protocol                    = "tcp"
+    cidr_blocks                 = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-aa1b34cf"
-  subnet_id = "${module.base_module.public_subnets[0]}"
-  instance_type = "t2.micro"
-  user_data = "${file("./bootstrap.sh")}"
+  ami                           = "${data.aws_ami.ubuntu.id}"
+  subnet_id                     = "${element(module.base_module.public_subnets, count.index)}"
+  associate_public_ip_address   = "true"
+  instance_type                 = "t2.micro"
+  security_groups               = ["${aws_security_group.web_sg.id}"]
+  user_data                     = "${file("./bootstrap.sh")}"
+  count                         = 4
+  tags {
+    Name                        = "training-web${count.index}"
+  }
 }
